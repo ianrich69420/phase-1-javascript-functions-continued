42bbe783aa8c9c1df3c8dfe6f2a177580f13cf58
ianrich69420 <ian.orieko@student.moringaschool.com>
2022-11-28 01:51:00 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun (activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork (activity = "go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective (punctuation = "*") {
+    return function (quality = "special") {
+        return `You are ${punctuation}${quality}${punctuation}!`
+    };
+}
+
